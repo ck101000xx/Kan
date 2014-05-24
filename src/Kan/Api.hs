f64ac5ddafb06a23b3979f270ba4f41bc4b5b615
ck101000xx <ck101000xx@gmail.com>
2014-05-24 22:44:15 +0800

    Kancolle Api

---
+++ src/Kan/Api.hs
@@ -0,0 +1,70 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE FlexibleContexts #-}
+module Kan.Api
+  ( module Kan.Api.Types
+  , runApiT
+  , api
+  ) where
+
+import Kan.Api.Types
+import Control.Exception
+import Control.Monad
+import Control.Monad.Trans
+import Control.Monad.Reader
+import Control.Monad.Error
+import Data.Aeson
+import Data.ByteString.Char8 as B
+import qualified Data.ByteString.Lazy as L
+import Data.Char (ord)
+import Data.Default
+import Data.Either
+import Data.Monoid
+import Network.HTTP.Client
+import Network.HTTP.Types
+
+runApiT :: Env -> ApiT m a -> m (Either ApiError a)
+runApiT env = runErrorT . flip runReaderT env . unApiT
+
+makeRequest :: (MonadReader Env m) =>
+  ByteString -> -- path
+  [(ByteString, ByteString)] -> -- params
+  m Request
+
+makeRequest path params = do
+  (server, token) <- asks $ liftM2 (,) server token
+  let extra =  [("api_token", token), ("api_verno", version)]
+  return $
+    urlEncodedBody (extra ++ params) $
+      def
+        { host = server
+        , path = "/kcsapi" <> path
+        , requestHeaders =(hReferer, "http://" <> server) : requestHeaders def }
+  where
+    version = "1"
+
+sendRequest ::
+  ( MonadReader Env (t m)
+  , MonadError ApiError (t m)
+  , MonadIO m
+  , MonadTrans t
+  ) =>
+  Request -> t m (L.ByteString)
+sendRequest req =
+  asks manager >>=
+  lift . liftIO . try . httpLbs req >>=
+  either (throwError . HttpError) (return . responseBody)
+
+parseResponse :: (MonadError ApiError m, FromJSON a) => L.ByteString -> m (ApiResponse a)
+parseResponse body = do
+  let svdata = L.dropWhile (/= (fromIntegral . ord) '{') body
+  either (throwError . ParseError) return $ eitherDecode svdata
+
+api :: (MonadIO m, FromJSON a) =>
+  ByteString -> -- path
+  [(ByteString, ByteString)] -> -- params
+  ApiT m a
+api path params = do
+  response <- makeRequest path params >>= sendRequest >>= parseResponse
+  when (apiResult response /= 1) $ throwError (KCSError (apiResultMsg response))
+  return $ apiData response
+
---
+++ src/Kan/Api/Types.hs
@@ -0,0 +1,48 @@
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE OverloadedStrings #-}
+module Kan.Api.Types where
+import Data.Aeson
+import Data.ByteString.Char8
+import Control.Applicative
+import Control.Monad.Trans
+import Control.Monad.Reader
+import Control.Monad.Error
+import Network.HTTP.Client (Manager, HttpException)
+
+type Token = ByteString
+
+data ApiError
+  = KCSError String
+  | ParseError String
+  | HttpError HttpException
+  | UnknownError String
+  deriving (Show)
+
+instance Error ApiError where
+  strMsg = UnknownError
+
+data Env = Env
+  { manager :: Manager
+  , token :: ByteString
+  , server :: ByteString
+  }
+
+newtype ApiT m a = ApiT
+  { unApiT :: ReaderT Env (ErrorT ApiError m) a
+  } deriving (Functor, Monad, MonadReader Env, MonadError ApiError, MonadIO)
+
+instance MonadTrans ApiT where
+  lift = ApiT . lift . lift 
+data ApiResponse a = ApiResponse
+  { apiResult :: Int
+  , apiResultMsg :: String
+  , apiData :: a
+  }
+
+instance (FromJSON a) => FromJSON (ApiResponse a) where
+  parseJSON (Object v) =
+    ApiResponse <$>
+      v .: "api_result" <*>
+      v .: "api_result_msg" <*>
+      v .: "api_data"
+
