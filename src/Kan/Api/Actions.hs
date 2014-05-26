a8fac6f0f7fab1e824b008276d2e93a451ef523f
ck101000xx <ck101000xx@gmail.com>
2014-05-26 19:38:58 +0800

    Remove function apiPort

--- src/Kan/Api/Actions.hs
+++ src/Kan/Api/Actions.hs
@@ -25,20 +25,3 @@ apiBasic = getMember "/basic" []
 apiShip :: (MonadIO m) => ApiT m [Ship]
 apiShip = getMember "ship" []
 
-apiPort :: (Functor m, MonadIO m, FromJSON a) => ApiT m a
-apiPort = do
-  fmap memberId basic
-  portNum <- fmap memberId basic >>= fmap (lift . liftIO) getPortNum
-  api "/api_port/port" [("api_port", portNum), ("api_sort_key", "5"), ("spi_sort_order", "2")]
-  where
-    getPortNum memberId = do
-      time <- fmap floor getPOSIXTime
-      return . pack $
-        '1' : show (memberId `mod` 1000) ++
-        show ((9999999999 - time - memberId) * array !! (memberId `mod` 10)) ++
-        suffix
-      where
-        suffix = "1000"
-        array = [1802, 9814, 5616, 4168, 7492, 5188, 2753, 8118, 6381, 7636]
-
-
--- src/Kan/Api/Actions/Types.hs
+++ src/Kan/Api/Actions/Types.hs
@@ -9,30 +9,9 @@ data Basic = Basic
 
 deriveApiData ''Basic
 
-class ShipList a where
-  ship :: a -> [Ship]
-
 data Ship = Ship
   { id :: Int
   } deriving (Show)
 
-data DeckPort = DeckPort
-  { deckPort_ship :: [Ship]
-  } deriving (Show)
-
-instance ShipList DeckPort where
-  ship = deckPort_ship
-
-data Port = Port
-  { port_ship :: [Ship]
-  , deckPort :: [DeckPort]
-  -- TODO
-  } deriving (Show)
-
-instance ShipList Port where
-  ship = port_ship
-
 deriveApiData ''Ship
-deriveApiData ''DeckPort
-deriveApiData ''Port
 
